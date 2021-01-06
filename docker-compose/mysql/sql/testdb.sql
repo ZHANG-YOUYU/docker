29f7f542764a6a9e4ee1a0ed601ba04f1550a362
ZHANG-YOUYU <77068573+ZHANG-YOUYU@users.noreply.github.com>
2021-01-07 01:19:06 +0800

    Add files via upload

---
+++ docker-compose/mysql/sql/testdb.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `testdb`;
+USE `testdb`;
+
+CREATE TABLE `people` (
+  `Name` varchar(50) NOT NULL,
+  `Age` int(11) NOT NULL
+) DEFAULT CHARSET=utf8;
+
+INSERT INTO `people` (`Name`, `Age`) VALUES ('ZHANG-YOUYU', 21), ('b10713092', 6), ('docker', 20);
